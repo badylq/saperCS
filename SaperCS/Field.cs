29ad666973b5b432967e8e08fe46f685600c22df
Krzysztof Zemlik <badylq@outlook.com>
2014-12-03 12:38:25 +0100

    base

---
+++ SaperCS/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SaperCS
+{
+	class Field
+	{
+		public bool isMine { get; private set; }
+
+	}
+}
